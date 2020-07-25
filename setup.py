3d0279f7455357a9502fd866cc8e29efb89b472d
Nicolas Lhomme <nlhomme@users.noreply.github.com>
2020-07-25 10:44:48 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup, Extension
 
-c_ext = Extension("tgvoip", ["tgvoip.cpp",], libraries=['tgvoip-dev',])
+c_ext = Extension("tgvoip", ["tgvoip.cpp",], libraries=['tgvoip',])
 
 setup(
     name='pytgvoip',
